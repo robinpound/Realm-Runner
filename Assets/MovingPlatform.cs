cbcb3d1c3bc7142a54fc6a3b02b1b05fedf0f30d
Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
2023-05-31 16:51:39 +0100

    Grey Boxing of main area + connection to tower
    Grey Boxing of main area + connection to tower

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -5,13 +5,78 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     [SerializeField]
-    private Transform waypointA;
+    private Transform waypointA, waypointB;
+    
+    private float platformLerpSpeed = .5f;
+    private enum PlatformDirectionSM { toPointA, toPointB }
+    private PlatformDirectionSM direction = PlatformDirectionSM.toPointA;
+
+    private Transform tempTransform;
+    private GameObject player;
 
 
-    public void MovePlatform()
+    private void FixedUpdate()
     {
-        Debug.Log("Platform is moving");
-        transform.position = Vector3.Lerp(transform.position, waypointA.position, 3f * Time.deltaTime);
+        float distanceToWaypointA = Vector3.Distance(transform.position, waypointA.position);
+        //Debug.Log("Distance to waypoint A " + distanceToWaypointA);
+        float distanceToWaypointB = Vector3.Distance(transform.position, waypointB.position);
+        //Debug.Log("Distance to waypoint B " + distanceToWaypointB);
+
+        switch (direction)
+        {
+            case PlatformDirectionSM.toPointA:
+                MovePlatformToWaypointA();
+                float switchFromWaypointAThreshold = 1f;
+                if (distanceToWaypointA < switchFromWaypointAThreshold)
+                    direction = PlatformDirectionSM.toPointB;
+                break;
+
+            case PlatformDirectionSM.toPointB:
+                MovePlatformToWaypointB();
+                float switchFromWaypointBThreshold = 1.5f;
+                if (distanceToWaypointB < switchFromWaypointBThreshold)
+                    direction = PlatformDirectionSM.toPointA;
+                break;
+        }
+            
     }
   
+    private void MovePlatformToWaypointA()
+    {
+        //Debug.Log("Platform is moving");
+        //transform.Translate(waypointA.transform.position * platformLerpSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, waypointA.position, platformLerpSpeed * Time.deltaTime);
+    }
+    private void MovePlatformToWaypointB()
+    {
+        //Debug.Log("Platform is moving");
+        //transform.Translate(waypointB.transform.position * platformLerpSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, waypointB.position, platformLerpSpeed * Time.deltaTime);
+    }
+
+    // Check if player is on platform and then change it to a child of the moving platform.
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collison with " + collision.gameObject.name);
+            ChangeParentofPlayerObjectToThis();
+        }
+        else return;
+    }
+
+    // Store player parent into the temp transform variable.
+    // Set the player to a child of this game object.
+    void ChangeParentofPlayerObjectToThis()
+    {
+        Debug.Log("Set player as child of moving platform");
+        tempTransform = player.transform.parent;
+        player.transform.parent = transform;
+    }
+
+    //Revert the parent of the player.
+    void RevertParent()
+    {
+        player.transform.parent = tempTransform;
+    }
 }
