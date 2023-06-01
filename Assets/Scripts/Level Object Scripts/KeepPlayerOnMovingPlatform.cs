e8be360f9a94998ea3f2e53ffadd105d99747093
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-06-01 12:03:09 +0100

    Updated moving platform script to work with triggers
    + Player script needs movement to be put into update method to work - left for Miguel to do.

--- Assets/Scripts/Level Object Scripts/KeepPlayerOnMovingPlatform.cs
+++ Assets/Scripts/Level Object Scripts/KeepPlayerOnMovingPlatform.cs
@@ -6,22 +6,48 @@ public class KeepPlayerOnMovingPlatform : MonoBehaviour
 {
     const string PLAYER = "Player";
 
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name + " is ON the moving platform");
+        if (other.gameObject.CompareTag(PLAYER))
+        {
+            other.gameObject.transform.SetParent(transform);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.gameObject.name + " is OFF the moving platform");
+        if (other.gameObject.CompareTag(PLAYER))
+        {
+            other.gameObject.transform.SetParent(null);
+        }
+        
+        
+    }
+    /* Old Code used with collisions
     private void OnCollisionEnter(Collision collision)
     {
+        
+        Debug.Log(collision.collider.name + " is ON the moving platform");
         if (collision.collider.CompareTag(PLAYER))
         {
-            Debug.Log(collision.collider.name + " is ON the moving platform");
+            
             collision.collider.transform.SetParent(transform);
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
+        Debug.Log(collision.collider.name + " is OFF the moving platform");
         if (collision.collider.CompareTag(PLAYER))
         {
-            Debug.Log(collision.collider.name + " is OFF the moving platform");
+            
             collision.collider.transform.SetParent(null);
         }
     }
+    */
+
 
 }
