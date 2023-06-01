e2332ae70d613b4c0fb0130ed9a8633987874d6f
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-06-01 22:26:09 +0100

    Created Hub Scene, added scenes to build index, added portal logic
    Scenes added to build index - Hub and tutorial levels from Mikes folder.

---
+++ Assets/Scripts/HubWorld - Mike/RealmPortalToTutorialLevel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RealmPortalToTutorialLevel : MonoBehaviour
+{
+    private const string PLAYER = "Player";
+
+    // If player enters trigger, load tutorial level.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(PLAYER))
+        {
+            SceneManager.LoadScene("TutorialLevelMikeRev1"); // Change string to build index TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        }
+    }
+}
