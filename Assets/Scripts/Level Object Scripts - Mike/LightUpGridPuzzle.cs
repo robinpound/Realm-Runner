208e2572caa2774dfdc9d85e4ec089c4bb5098a1
Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
2023-06-06 17:08:32 +0100

    Test of light puzzle

---
+++ Assets/Scripts/Level Object Scripts - Mike/LightUpGridPuzzle.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LightUpGridPuzzle : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject light;
+    private GameObject instantiatedLight;
+    Vector3 rayCastVector;
+    private const string PLAYER = "Player";
+    private bool hasLightSpawned = false;
+
+    private void Start()
+    {
+        Vector3 lightSpawnOffsetY = new Vector3(0f, .5f, 0f);
+        instantiatedLight = Instantiate(light, transform.position + lightSpawnOffsetY, Quaternion.identity);
+        instantiatedLight.SetActive(false);
+    }
+
+    private void Update()
+    {
+        Debug.Log("Light " + hasLightSpawned);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("eagle has landed");
+        if (other.gameObject.CompareTag(PLAYER) && hasLightSpawned == false)
+        {
+            instantiatedLight.SetActive(true);
+            hasLightSpawned=true;
+            
+        }
+        /*
+        if (other.gameObject.CompareTag(PLAYER) && hasLightSpawned == true)
+        {
+            Debug.Log("hasLightSpawned");
+            instantiatedLight.SetActive(false);
+            hasLightSpawned = false;
+        }*/
+        
+    }
+
+
+
+
+
+
+
+
+    /*
+    
+    private void Update()
+    {
+        DrawCubeRayCast();
+        Debug.Log("Light " + hasLightSpawned);
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, rayCastVector, out hit))
+        {
+            Debug.Log(hit.collider.name + " detected");
+
+            if (hit.collider.CompareTag(PLAYER) && hasLightSpawned == false)
+            {
+                instantiatedLight.SetActive(true);
+                hasLightSpawned = true;
+            }
+            
+            
+        }
+    }
+
+    private void DrawCubeRayCast()
+    {
+        float rayCastLength = 2f;
+        rayCastVector = transform.TransformDirection(Vector3.up) * rayCastLength;
+        Debug.DrawRay(transform.position, rayCastVector, Color.magenta);
+    }
+    */
+}
