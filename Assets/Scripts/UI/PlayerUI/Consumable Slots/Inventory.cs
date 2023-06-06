7e9d385ba6c9bac8e09c7d310da91511da0293c5
Liam <wliam4410@gmail.com>
2023-06-06 12:06:47 +0100

    Collectables
    Fragments, Coins, Consumables

--- Assets/Scripts/UI/PlayerUI/Consumable Slots/Inventory.cs
+++ Assets/Scripts/UI/PlayerUI/Consumable Slots/Inventory.cs
@@ -14,6 +14,8 @@ public class Inventory : MonoBehaviour
 
     private GameObject one;
     private GameObject two;
+    public bool slot1Bool;
+    public bool slot2Bool;
     public int arrayPos1;
     public int arrayPos2;
 
@@ -26,25 +28,76 @@ public class Inventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            SpawnObjects();
+        }
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            GameObject.Destroy(one);
-            GameObject.Destroy(two);
-            one = Instantiate(items[arrayPos1], slot1.transform.position, transform.rotation); ;
-            one.transform.SetParent(slot1Parent.transform);
+            if(arrayPos1 >= 1)
+            {
+                GameObject.Destroy(one);
+                GameObject.Destroy(two);
+                one = Instantiate(items[arrayPos1], slot1.transform.position, transform.rotation); ;
+                one.transform.SetParent(slot1Parent.transform);
+                one.gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+                slot1Bool = true;
 
-            two = Instantiate(items[arrayPos2], slot2.transform.position, transform.rotation); ;
-            two.transform.SetParent(slot2Parent.transform);
+
+                two = Instantiate(items[arrayPos2], slot2.transform.position, transform.rotation); ;
+                two.transform.SetParent(slot2Parent.transform);
+                two.gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+                slot2Bool = true;
+            }
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            GameObject.Destroy(one);
-            GameObject.Destroy(two);
-            one = Instantiate(items[arrayPos2], slot1.transform.position, transform.rotation); ;
-            one.transform.SetParent(slot1Parent.transform);
+            if(arrayPos2 >= 1)
+            {
+                GameObject.Destroy(one);
+                GameObject.Destroy(two);
+                two = Instantiate(items[arrayPos1], slot2.transform.position, transform.rotation); ;
+                two.transform.SetParent(slot2Parent.transform);
+                two.gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+                slot2Bool = true;
 
-            two = Instantiate(items[arrayPos1], slot2.transform.position, transform.rotation); ;
+                one = Instantiate(items[arrayPos2], slot1.transform.position, transform.rotation); ;
+                one.transform.SetParent(slot1Parent.transform);
+                one.gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+                slot1Bool = true;
+            }
+        }
+    }
+
+    public void SpawnObjects()
+    {
+        if (arrayPos1 == 1)
+        {
+            one = Instantiate(items[arrayPos1], slot1.transform.position, transform.rotation); ;
+            one.transform.SetParent(slot1Parent.transform);
+            one.gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+            slot1Bool = true;
+        }
+        else if(arrayPos1 == 2)
+        {
+            one = Instantiate(items[arrayPos1], slot1.transform.position, transform.rotation); ;
+            one.transform.SetParent(slot1Parent.transform);
+            one.gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+            slot1Bool = true;
+        }
+        if(arrayPos2 == 1)
+        {
+            two = Instantiate(items[arrayPos2], slot2.transform.position, transform.rotation); ;
             two.transform.SetParent(slot2Parent.transform);
+            two.gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+            slot2Bool = true;
+        }
+        else if (arrayPos2 == 2)
+        {
+            two = Instantiate(items[arrayPos2], slot2.transform.position, transform.rotation); ;
+            two.transform.SetParent(slot2Parent.transform);
+            two.gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+            slot2Bool = true;
         }
     }
 }
