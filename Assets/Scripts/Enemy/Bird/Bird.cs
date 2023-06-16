d382835bba24b93d913327a2ae7faf71b2a62270
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-06-16 22:25:43 +0100

    Imported Robins enemies and deleted old enemy file
    + Walking enemies
    + Flying enemies
    + Enemy Test Scene

---
+++ Assets/Scripts/Enemy/Bird/Bird.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Bird : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Transform player; 
+
+    [Header("Alerted!:")] 
+    [SerializeField] private float attackRange;
+    [SerializeField] private float delayBeforeReset;
+    [SerializeField] private float alertSightRange;
+    [SerializeField] private float alertSpeed;
+    [SerializeField] private float escapeSpeed;
+
+    private bool isplayerInAlertRange = false;
+    private bool isplayerInAttackRange = false;
+    [SerializeField] private bool justAttacked = false;
+    
+
+    [Header("Layers")] 
+    [SerializeField] private LayerMask whatIsPlayer;
+
+    [Header("Patrolling!:")] 
+    [SerializeField] GameObject patrolPoint;
+    [SerializeField] float patrolPointRange; 
+    [SerializeField] float patrolSpeedmultiplier;
+    [SerializeField] float patrolwaypointAcceptaceRadius; 
+    private Vector3 waypoint; 
+    private bool isWaypointSet = false;
+
+    private void Start() 
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void FixedUpdate() 
+    {
+        float disctance = (player.position - transform.position).magnitude;
+
+        isplayerInAlertRange = Physics.CheckSphere(transform.position, alertSightRange, whatIsPlayer);
+        isplayerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!isplayerInAlertRange) Patrol(); 
+        if (isplayerInAlertRange) MoveToPlayer(); 
+
+    }
+
+    private void Patrol()
+    {
+        justAttacked = false;
+        if(!isWaypointSet) FindWayPoint(); 
+        if(isWaypointSet) 
+        {
+            transform.LookAt(waypoint);
+            rb.AddRelativeForce(Vector3.forward * patrolSpeedmultiplier, ForceMode.Force);
+            
+            //rb.AddForceAtPosition(Vector3.forward * patrolSpeedmultiplier, transform.position, ForceMode.Force);
+        }
+        
+        WayPointReachedCheck();
+    }
+
+    private void FindWayPoint()
+    {
+        float randomZ = Random.Range(-patrolPointRange, patrolPointRange);
+        float randomX = Random.Range(-patrolPointRange, patrolPointRange);
+
+        Vector3 currentPos = patrolPoint.transform.position;
+        waypoint = new Vector3(currentPos.x + randomX, currentPos.y, currentPos.z + randomZ);
+        isWaypointSet = true;
+    }
+
+    private void WayPointReachedCheck()
+    {
+        Vector3 distanceToWalkPoint = transform.position - waypoint;
+        if (distanceToWalkPoint.magnitude < patrolwaypointAcceptaceRadius)
+        {
+            isWaypointSet = false;
+        }
+    }
+
+    private void MoveToPlayer()
+    {  
+        transform.LookAt(player);    
+        if (!isplayerInAttackRange && !justAttacked) //move towards player
+        {
+            rb.AddRelativeForce(Vector3.forward * alertSpeed, ForceMode.Force);
+            Invoke(nameof(Escape), delayBeforeReset);
+        }
+        if (isplayerInAttackRange) 
+        {
+            //Debug.Log("HIT!");
+            Escape();
+        }
+    }
+
+    private void Escape()
+    {
+        justAttacked = true;
+        rb.AddForce(Vector3.up * escapeSpeed);
+        Invoke(nameof(ResetAttack), delayBeforeReset);
+    }
+    
+    private void ResetAttack() => justAttacked = false;
+
+    private void OnDrawGizmosSelected() {
+        
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(patrolPoint.transform.position, patrolPointRange);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, alertSightRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.black;
+        Gizmos.DrawSphere(waypoint, 1f);
+        if (!isplayerInAlertRange && !isplayerInAttackRange)
+        {
+            Gizmos.color = Color.black;
+            Gizmos.DrawWireSphere(waypoint, patrolwaypointAcceptaceRadius);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/Walker/Walking.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class Walking : MonoBehaviour
+{
+    [Header("Don't Touch!")]    
+    [SerializeField] private NavMeshAgent agent; 
+    [SerializeField] private Transform player; 
+    [SerializeField] private LayerMask whatIsGround, whatIsPlayer;
+
+    [Header("Settings")]    
+    [SerializeField] private float waypointRange;
+    [SerializeField] private float chaseRange;
+    [SerializeField] private float attackRange;
+    [SerializeField] private float timeBetweenAttacks; 
+    [SerializeField] private float patrolSpeed = 0.5f;
+    [SerializeField] private float chaseSpeed = 1f;
+    [SerializeField] private float attackSpeed = 1.5f;
+    
+    [Header("Behind the scenes:")] 
+    [SerializeField] private Vector3 waypoint; 
+    [SerializeField] private bool isWaypointSet = false;
+    [SerializeField] private bool isplayerInChaseRange = false;
+    [SerializeField] private bool isplayerInAttackRange = false;
+    [SerializeField] private bool isattacked = false; 
+
+    void Awake() 
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void FixedUpdate() {
+        isplayerInChaseRange = Physics.CheckSphere(transform.position, chaseRange, whatIsPlayer);
+        isplayerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!isplayerInChaseRange && !isplayerInAttackRange) Patrol(); 
+        if (isplayerInChaseRange && !isplayerInAttackRange) Chase(); 
+        if (isplayerInChaseRange && isplayerInAttackRange) Attack(); 
+    }
+    
+    private void Patrol()
+    {
+        agent.speed = patrolSpeed;
+        if(!isWaypointSet) FindWayPoint(); 
+        if(Random.Range(0,100) == 1) FindWayPoint();
+
+        if(isWaypointSet)
+            agent.SetDestination(waypoint);
+
+        //Check if reached waypoint
+        Vector3 distanceToWalkPoint = transform.position - waypoint;
+        if (distanceToWalkPoint.magnitude < 1f)
+            isWaypointSet = false;
+    }
+
+    private void FindWayPoint()
+    {
+        float randomZ = Random.Range(-waypointRange, waypointRange);
+        float randomX = Random.Range(-waypointRange, waypointRange);
+        float currentZ = transform.position.z;
+        float currentX = transform.position.x;
+        
+        waypoint = new Vector3 (currentX + randomX, transform.position.y, currentZ + randomZ);
+        //check if place is valid
+        
+        if(Physics.Raycast(waypoint, -transform.up, 2f, whatIsGround)){
+            isWaypointSet = true;
+        }
+    }
+
+    private void Chase()
+    {
+        agent.speed = chaseSpeed;
+        Vector3 playerposition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        agent.SetDestination(player.transform.position);
+    }
+
+    private void Attack()
+    {
+        agent.speed = attackSpeed;
+        agent.SetDestination(transform.position); //Stop moving
+        transform.LookAt(player);
+
+        if(!isattacked)
+        {
+            /*
+                ATTACK CODE HERE!
+            */
+            isattacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack() {
+        isattacked = false;
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, waypointRange);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawSphere(waypoint, 0.5f);
+    }
+}
