f88bd1c0bde4b57c9a4c25aeb7d16cae25eb20ba
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-06-24 19:45:35 +0100

    Created a script to choose and spawn a target at a random point

--- Assets/_Mike/Scripts-M/Shooting Target/ShootingTimeTrialManager.cs
+++ Assets/_Mike/Scripts-M/Shooting Target/ShootingTimeTrialManager.cs
@@ -3,38 +3,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+using Unity.VisualScripting;
+using System.Runtime.CompilerServices;
 
 public class ShootingTimeTrialManager : MonoBehaviour
 {
+    [Header("Debugs")]
     [SerializeField]
     private int points = 0;
-    private float timeRemaining = 10f;
-
-    [SerializeField]
-    private bool isTimerRunning;
     [SerializeField]
     private TMP_Text timeText;
+
+    [Header("Timer Settings")]
+    [SerializeField]
+    private float timeRemaining = 10f;
+    [SerializeField]
+    private bool isTimerRunning;
 
     private void Start()
     {
         isTimerRunning = true;
+        
     }
 
     private void Update()
     {
         Timer();
 
+        
+
         //Debug.Log("Player has " + points + " Points");
         if (points >= 2)
         {
             Debug.Log("The Player Beat The Time Trial");
-            
+
             timeText.text = "Player Wins!";
-            
+
             // need to add puzzle triumph script containing + 1 Fragment + PlayTriumphSound
         }
     }
+
+    
 
     // Timer for time trial
     private void Timer()
@@ -64,8 +73,6 @@ public class ShootingTimeTrialManager : MonoBehaviour
         float minutes = Mathf.FloorToInt(timeDisplay / 60);
         float seconds = Mathf.FloorToInt(timeDisplay% 60);
         timeText.text = string.Format("{0:00}:{1:00}", minutes,seconds);
-
-        
     }
 
     public int pointsAdded(int pointsGained)
