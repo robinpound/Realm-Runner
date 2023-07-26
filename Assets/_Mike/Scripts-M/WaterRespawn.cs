4933754782aeadfde3f4f41e6a612425083bd244
JesusMiguel1 <jesusmiguelrp8@hotmail.com>
2023-07-26 12:07:11 +0100

    Respawn player if falls into the water

--- Assets/_Mike/Scripts-M/WaterRespawn.cs
+++ Assets/_Mike/Scripts-M/WaterRespawn.cs
@@ -7,6 +7,16 @@ public class WaterRespawn : MonoBehaviour
 {
     [SerializeField]
     private Transform player;
+    PlayerGravity gravity;
+    public Transform spawnPoint;
+    PlayerCharacterController controller;
+
+    private void Start()
+    {
+        gravity = GetComponent<PlayerGravity>();
+        controller = FindObjectOfType<PlayerCharacterController>();
+
+    }
 
     private void Update()
     {
@@ -15,14 +25,25 @@ public class WaterRespawn : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.tag + " hit the water");
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && gravity.currentMovement.y > -9.8)
         {
             Debug.Log(other.tag + " spawned at new point");
+            //gravity.currentMovement.y = 0;
+            controller.controller.enabled = false;
 
             //player.position += new Vector3(spawnPoint.position.x, player.position.y, spawnPoint.position.z);
-            //player.position = spawnPoint.position;
+            player.position = spawnPoint.position;
+            Debug.Log("GRAVITY..." + gravity.currentMovement.y);
             //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            player.GetComponent<PlayerStats>().InstaDead();
+            Debug.Log(player.position + " Spawn point poistion");
+            //player.GetComponent<PlayerStats>().InstaDead();
+
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        controller.controller.enabled = true;
+
     }
 }
