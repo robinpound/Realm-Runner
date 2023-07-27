80508c5a4ca48b5e6966c4aa3550b0fd75bfad63
Liam <wliam4410@gmail.com>
2023-07-27 16:37:48 +0100

    Controls Updated
    Edited the Bow and Arrow Scripts to Use the Input Actions Assets to  use the controls

--- Assets/_Liam/Scripts/WeaponEquipped.cs
+++ Assets/_Liam/Scripts/WeaponEquipped.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WeaponEquipped : MonoBehaviour
 {
+    InputActions input;
     [SerializeField] GameObject player;
 
     //Bow Objects
@@ -11,8 +13,11 @@ public class WeaponEquipped : MonoBehaviour
     [SerializeField] GameObject handBow;
     public bool bow;
 
+    [SerializeField] bool aiming;
+
     private void Awake()
     {
+        input = new InputActions();
         // Fetching player character
         player = GameObject.FindGameObjectWithTag("Player");
     }
@@ -25,16 +30,26 @@ public class WeaponEquipped : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        input.PlayerActions.ArrowAiming.started += Bool;
+        input.PlayerActions.ArrowAiming.performed += Bool;
+        input.PlayerActions.ArrowAiming.canceled += Bool;
+
+
         // Activate bow on aim
-        if (Input.GetMouseButtonDown(1))
+        if (aiming)
         {
             BowEquip();
         }
         // Deactivate bow on !aim
-        if (Input.GetMouseButtonUp(1))
+        if (!aiming)
         {
             BowDeequip();   
         }
+    }
+    private void Bool(InputAction.CallbackContext context)
+    {
+        aiming = context.ReadValueAsButton();
     }
     public void BowEquip()
     {
@@ -49,5 +64,14 @@ public class WeaponEquipped : MonoBehaviour
         // Swapping out GameObjects
         backBow.SetActive(true);
         handBow.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        input.PlayerActions.Enable();
+    }
+    private void OnDisable()
+    {
+        input.PlayerActions.Disable();
     }
 }
