b82b49c84fb9305d767d1ba648c3740390c84f3b
Robin Pound <54936470+robinpound@users.noreply.github.com>
2023-07-27 21:00:51 +0100

    Missile Import, Mini boss import, mike level change

---
+++ Assets/_Robin/miniboss/miniboss.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class miniboss : MonoBehaviour
+{
+    [SerializeField] Transform playerTransform;
+    [SerializeField] GameObject player;
+
+    miniboss_ref references;
+
+    float pathUpdateDeadLine;
+    float attackDeadLine;
+    float healDeadLine;
+
+    float alertDistance;
+    float shootingDistance;
+
+    public float temporaryHealth = 100; //TEMP
+    private float healingMinHealth = 30;
+
+    public GameObject missile;
+    public GameObject spawnpoint;
+
+    [SerializeField] private bool isDoingSomething = false;
+    
+    private enum EnemyState
+    {
+        waiting, 
+        MovingTo,
+        Shooting,
+        Healing
+    }
+
+    [SerializeField] private EnemyState currentState;
+
+    private void Awake() {
+        references = GetComponent<miniboss_ref>();
+    }
+
+    private void Start() {
+        alertDistance = references.alertDistance; //transfer these
+        shootingDistance = references.shootingDistance; //transfer these
+        currentState = EnemyState.waiting;
+    }
+
+    private void FixedUpdate() {
+
+        //bool needsHealing = temporaryHealth < 25; // CHANGE-TO: randomiser + if (references.health.healthpoints <= (totalhealth * 0.2))
+        //bool needsLeaping = temporaryHealth < 50; // CHANGE-TO: randomiser + ... ???
+
+        bool isLowHealth = temporaryHealth < healingMinHealth;
+        bool inAlertRange = Vector3.Distance(transform.position, playerTransform.position) <= alertDistance;
+        bool inShootRange = Vector3.Distance(transform.position, playerTransform.position) <= shootingDistance;
+        
+        
+        if (inAlertRange & !isDoingSomething) currentState = EnemyState.MovingTo;
+        if (inShootRange & !isDoingSomething) currentState = EnemyState.Shooting;
+        if (isDoingSomething) currentState = EnemyState.waiting; 
+            
+        if (isLowHealth) Heal(); 
+        
+        switch (currentState)
+        {
+            case EnemyState.waiting:
+                references.navMA.SetDestination(transform.position);
+                break;
+            case EnemyState.MovingTo:
+                LookAtPlayer();
+                UpdateNavPath();
+                break;
+            case EnemyState.Shooting:
+                LookAtPlayer();
+                Shoot();
+                break;
+            case EnemyState.Healing:
+                Heal(); 
+                break;
+        }        
+    }
+
+    private void UpdateNavPath(){
+        // NavMesh Delay optimisation 
+        if(Time.time >= pathUpdateDeadLine) 
+        {
+            pathUpdateDeadLine = Time.time + references.pathUpdateDelay;
+            references.navMA.SetDestination(playerTransform.position);
+        }
+    }
+
+    private void LookAtPlayer() {
+        Vector3 lookPosition = playerTransform.position - transform.position;
+        lookPosition.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPosition);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.2f);
+    }
+
+    private void Shoot()
+    {
+        if (Time.time >= attackDeadLine)
+        {
+            Instantiate(missile, spawnpoint.transform.position, Quaternion.identity);
+            references.navMA.SetDestination(transform.position);
+            attackDeadLine = Time.time + references.attackDelay;
+        }
+    }
+
+    private void Heal(){
+        isDoingSomething = true;
+        if(Time.time >= healDeadLine)
+        {
+            references.ps.Play();
+            temporaryHealth += 15;
+            healingMinHealth -= 5;
+            healDeadLine = Time.time + references.healDelay;
+            Invoke(nameof(ResetDoing), references.healDelay);
+        }
+    }
+    
+    private void ResetDoing() => isDoingSomething = false;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue; Gizmos.DrawWireSphere(transform.position, alertDistance);
+        Gizmos.color = Color.red;  Gizmos.DrawWireSphere(transform.position, shootingDistance);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("HIT by somehting");
+        temporaryHealth -= 10;
+    }
+}
