8c8dee18bfabf56f0dae02e06e5210be1e44b3f8
Liam <wliam4410@gmail.com>
2023-08-01 19:52:58 +0100

    Update PlayerUIHealth.cs

--- Assets/_Liam/Scripts/UI/PlayerUI/PlayerUIHealth.cs
+++ Assets/_Liam/Scripts/UI/PlayerUI/PlayerUIHealth.cs
@@ -21,43 +21,29 @@ public class PlayerUIHealth : MonoBehaviour
     void Update()
     {
         health = player.GetComponent<PlayerStats>().currentHealth;
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            health -= damage;
-        }
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            health = 5;
+            health = 3;
         }
 
         #region Main Hearts
-        else if (health <= 5 && health > 4)
+        else if (health <= 3 && health > 2)
         {
-            hearts[4].SetActive(true);
-            hearts[3].SetActive(true);
             hearts[2].SetActive(true);
             hearts[1].SetActive(true);
             hearts[0].SetActive(true);
         }
-        else if (health <= 4 && health > 3)
+        else if (health <= 2 && health > 1)
         {
             hearts[0].SetActive(false);
         }
-        else if (health <= 3 && health > 2)
+        else if (health <= 1 && health > 0)
         {
             hearts[1].SetActive(false);
         }
-        else if (health <= 2 && health > 1)
-        {
-            hearts[2].SetActive(false);
-        }
-        else if (health <= 1 && health > 0)
-        {
-            hearts[3].SetActive(false);
-        }
         else if (health <= 0)
         {
-            hearts[4].SetActive(false);
+            hearts[2].SetActive(false);
         }
         #endregion
 
