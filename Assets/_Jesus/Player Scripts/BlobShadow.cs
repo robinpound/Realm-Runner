c65a12682567fe7668217d4bd90c3de9c05cf704
JesusMiguel1 <jesusmiguelrp8@hotmail.com>
2023-08-05 00:48:53 +0100

    Needs to change the material of the blob shadow

---
+++ Assets/_Jesus/Player Scripts/BlobShadow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobShadow : MonoBehaviour
+{
+    [SerializeField] GameObject fakeShadow;
+    PlayerCharacterController controller;
+    bool groundCheck;
+
+    private void Awake()
+    {
+        //controller = FindObjectOfType<PlayerCharacterController>();
+        //fakeShadow.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+            {
+                Vector3 position = hit.point;
+                position.y += 0.01f;
+                fakeShadow.transform.position = position;
+
+
+            }
+    }
+
+
+}
