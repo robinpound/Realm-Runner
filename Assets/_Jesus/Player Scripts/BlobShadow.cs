5188bd3b799967ecc26e8733f52d1b5b03114f49
JesusMiguel1 <jesusmiguelrp8@hotmail.com>
2023-08-05 17:55:51 +0100

    Blob shadow added to the player to indicate where is landing when jump

---
+++ Assets/_Jesus/Player Scripts/BlobShadow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobShadow : MonoBehaviour
+{
+    [SerializeField] GameObject fakeShadow;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+        {
+            Vector3 position = hit.point;
+            position.y += 0.01f;
+            fakeShadow.transform.position = position;
+        }
+    }
+
+}
